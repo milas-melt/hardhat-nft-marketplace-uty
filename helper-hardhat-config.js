161a419d4cce301edafb1c6da62d59d72b54b0c1
milas-melt <tlemcani.salim00@gmail.com>
2022-08-16 13:01:46 +0100

    push latest updates

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -8,7 +8,7 @@ const networkConfig = {
         subscriptionId: "588",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
-        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH --> IRRELEVANT FOR MARKETPLACE !
         callbackGasLimit: "500000", // 500,000 gas
     },
     4: {
